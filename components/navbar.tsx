a4299e2c7f95a4c6b5eb5fc9e8c74ac0ebd8ac45
ahmid <ahmidsanni@gmail.com>
2026-09-18 13:12:00 +0100

    fix: hamburger menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="fixed inset-x-0 top-0 z-50 box-border max-w-full md:rounded-b-lg p-1 md:p-0.5 bg-white text-sm text-background xl:border-4 xl:text-base">
+    <header className="fixed left-0 right-0  top-0 z-50 box-border w-full md:rounded-b-lg p-1 md:p-0.5 bg-white text-sm text-background xl:p-1 xl:text-base">
       <div className="flex items-center justify-between rounded-xl bg-muted">
         <div className="2xl:container 2xl:mx-auto px-4 sm:px-10 lg:px-14 xl:px-20 py-2.5 xl:py-3 flex items-center justify-between gap-3 xl:gap-4  w-full">
           <div className="">
